5ed2fbbd417e197f889706a0b1b773abad07a081
Jonathan Fok kan <jfokkan@inocybe.com>
2014-12-24 13:03:03 -0500

    REMOVED extra exit

--- build.sh
+++ build.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-cp set_inocybe_shared_user_password.sh src/scripts
-./chromite/bin/cros_sdk --create
+cp set_inocybe_shared_user_password.sh ../src/scripts
+../chromite/bin/cros_sdk --create
 
 # Don't forget ';'s after each command
-./chromite/bin/cros_sdk -- /bin/bash -c "
+../chromite/bin/cros_sdk -- /bin/bash -c "
 cd ~/trunk/src/scripts/;
 pwd;
 ./set_inocybe_shared_user_password.sh;
 echo amd64-usr > .default_board;
-exit;
 ./setup_board --force;
 ./build_packages;
 ./build_image prod --group alpha;
 exit;
 "
 
-rm src/scripts/set_inocybe_shared_user_password.sh
-
-# $1$rYphZpcg$rSjCYRMVQYHjTJTVNnwTb.
+rm ../src/scripts/set_inocybe_shared_user_password.sh
