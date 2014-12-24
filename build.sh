441428f520af04317a35d4264f133f07f9dfd765
Jonathan Fok kan <jfokkan@inocybe.com>
2014-12-24 16:29:31 -0500

    MOVED build process in chrooted env to cr_build.sh cr_build.sh is then run by build.sh

--- build.sh
+++ build.sh
@@ -6,32 +6,17 @@ CROSSDK="./chromite/bin/cros_sdk"
 
 cp -r /var/go-agent-coreos-resources .
 cp spore-auto-build/set_inocybe_shared_user_password.sh ./src/scripts
+cp spore-auto-build/cr_build.sh ./src/scripts
 ./chromite/bin/cros_sdk --create
 
 # Don't forget ';'s after each command
 ./chromite/bin/cros_sdk -- /bin/bash -c "
-cd ~/trunk/go-agent-coreos-resources;
-cp id_rsa id_rsa.pub ~/.ssh;
-cp update-payload-key.* /usr/share/update_engine/;
-cd ~/trunk/src/scripts/;
-pwd;
-./set_inocybe_shared_user_password.sh;
-echo amd64-usr > .default_board;
-./setup_board --force;
-./build_packages;
-./build_image prod --group alpha;
-#install awscli for use of ino_core_roller_upload
-cd ~/trunk/src; mkdir aws-cli; cd aws-cli;
-wget https://bootstrap.pypa.io/get-pip.py;
-sudo python get-pip.py;
-sudo pip install awscli;
-cd ~/trunk/go-agent-coreos-resources;
-mkdir ~/.aws
-cp credentials ~/.aws/;
-#run ino_core_roller_upload
-cd ~/trunk/src/scripts/;
+./cr_build.sh;
 exit;
 "
 
+#run ino_core_roller_upload
+
 rm ./src/scripts/set_inocybe_shared_user_password.sh
+rm ./src/scripts/cr_build.sh
 rm -rf ./go-agent-coreos-resources
---
+++ cr_build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e -x
+
+# ssh credentials
+cd ~/trunk/go-agent-coreos-resources;
+cp id_rsa id_rsa.pub ~/.ssh;
+# signing keys
+sudo cp update-payload-key.* /usr/share/update_engine/;
+cd ~/trunk/src/scripts/;
+# set inocybe password
+./set_inocybe_shared_user_password.sh;
+#build board, packages, image
+echo amd64-usr > .default_board;
+./setup_board --force;
+./build_packages;
+./build_image prod --group alpha;
+#install aws
+cd ~/trunk/src; mkdir aws-cli; cd aws-cli;
+wget https://bootstrap.pypa.io/get-pip.py;
+sudo python get-pip.py;
+sudo pip install awscli;
+cd ~/trunk/go-agent-coreos-resources;
+cp credentials ~/.aws/;
+cd ~/trunk/src/scripts/;
+# run ino_core_roller_upload
