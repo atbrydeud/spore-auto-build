7c889570e1ec622cf76a636e51740a4653cf1add
Jonathan Fok kan <jfokkan@inocybe.com>
2014-12-29 13:44:10 -0500

    ADDED NOS-update-client to cr_build.sh

--- cr_build.sh
+++ cr_build.sh
@@ -8,10 +8,12 @@ cd ~/trunk/go-agent-coreos-resources;
 cp id_rsa id_rsa.pub ~/.ssh;
 chmod 600 ~/.ssh/id_rsa
 chmod 644 ~/.ssh/id_rsa.pub
+
 # signing keys
 echo -e '\nAdding signing keys for update_engine\n'
 sudo cp update-payload-key.* /usr/share/update_engine/;
 cd ~/trunk/src/scripts/;
+
 # Install pfm-management-client and replace updateservicectl
 cd ~/trunk/src/third_party
 if [ ! -d ~/trunk/src/third_party/pfm-management-client ]; then
@@ -21,16 +23,28 @@ if [ ! -d ~/trunk/src/third_party/pfm-management-client ]; then
     sudo rm /usr/bin/updateservicectl
     sudo cp bin/updateservicectl /usr/bin/updateservicectl
 fi
-cd ~/trunk/src/scripts/;
+
+# Get NOS-update-client and place it at ~/trunk/src/coreos
+cd ~/trunk/src/third_party
+git clone ssh://go-agent@review.inocybe.com:29418/NOS-update-client
+cd NOS-update-client
+./build
+mkdir ~/trunk/third_party/coreos-overlay/app-misc/NOS-update-client/
+cp -r ebuild/* ~/trunk/third_party/coreos-overlay/app-misc/NOS-update-client/
+
 # set inocybe password
+cd ~/trunk/src/scripts/;
 echo -e '\nSetting inocybe password for user account:inocybe\n'
 ./set_inocybe_shared_user_password.sh;
+
 #build board, packages, image
+cd ~/trunk/src/scripts/;
 echo -e '\nBuilding image\n'
 echo amd64-usr > .default_board;
 ./setup_board --force;
 ./build_packages;
 ./build_image prod --group alpha;
+
 #install aws
 echo -e '\n Installing AWS\n'
 if [ ! -d ~/trunk/src/aws-cli ]; then
