6855cb81166411f1f8691c17166f8b9ffebaf2ba
Jonathan Fok kan <jfokkan@inocybe.com>
2014-12-29 10:48:01 -0500

    ADDED pfm-management-client to cr_build.sh

--- cr_build.sh
+++ cr_build.sh
@@ -3,19 +3,28 @@
 set -e -x
 
 # ssh credentials
+echo -e '\nAdding ssh credentials for review.inocybe.com\n'
 cd ~/trunk/go-agent-coreos-resources;
 cp id_rsa id_rsa.pub ~/.ssh;
 # signing keys
+echo -e '\nAdding signing keys for update_engine\n'
 sudo cp update-payload-key.* /usr/share/update_engine/;
 cd ~/trunk/src/scripts/;
+# Install pfm-management-client
+cd ~/trunk/src/third_party
+git clone http://review.inocybe.com/pfm-management-client
+cd ~/trunk/src/scripts/;
 # set inocybe password
+echo -e '\nSetting inocybe password for user account:inocybe\n'
 ./set_inocybe_shared_user_password.sh;
 #build board, packages, image
+echo -e '\nBuilding image\n'
 echo amd64-usr > .default_board;
 ./setup_board --force;
 ./build_packages;
 ./build_image prod --group alpha;
 #install aws
+echo -e '\n Installing AWS\n'
 cd ~/trunk/src; mkdir aws-cli; cd aws-cli;
 wget https://bootstrap.pypa.io/get-pip.py;
 sudo python get-pip.py;
