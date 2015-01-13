f2d6f03d3e51bdb15d28bec9e97cbc54f96031ae
Jonathan Fok kan <jfokkan@inocybe.com>
2015-01-13 13:15:24 -0500

    ADDED sdk delete on each build

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ CROSSDK="./chromite/bin/cros_sdk"
 cp -r /var/go-agent-coreos-resources .
 cp spore-auto-build/set_inocybe_shared_user_password.sh ./src/scripts
 cp spore-auto-build/cr_build.sh ./src/scripts
+./chromite/bin/cros_sdk --delete
 ./chromite/bin/cros_sdk --create
 
 # Don't forget ';'s after each command
